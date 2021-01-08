74acecad285bc99bca01f80d2fea31e40c53055b
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-01-08 09:56:20 -0500

    progress

--- perl/08.pl
+++ perl/08.pl
@@ -8,6 +8,8 @@
 #
 
 # We use %win hash to store window parameters
+use strict;
+use warnings;
 
 use Curses;
 
@@ -19,7 +21,8 @@ keypad(1);
 noecho();
 init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
-%win = ();
+my $_DEBUG = 0;
+my %win = ();
 init_win_params(\%win);
 print_win_params(\%win);
 
@@ -29,7 +32,7 @@ refresh();
 attroff(COLOR_PAIR(1));
 
 create_box(\%win, 1);
-
+my $ch;
 while (($ch = getch()) != KEY_F(1)) {
     if ($ch == KEY_LEFT) {
 	create_box(\%win, 0);
@@ -56,25 +59,25 @@ while (($ch = getch()) != KEY_F(1)) {
 endwin();
 
 sub init_win_params {
-    $p_win = shift;
+    my $p_win = shift;
 
-    $$p_win{'height'} = 3;
-    $$p_win{'width'} = 10;
-    $$p_win{'starty'} = ($LINES - $p_win{'height'}) / 2;
-    $$p_win{'startx'} = ($COLS - $p_win{'width'}) / 2;
-    $$p_win{'ls'} = '|';
-    $$p_win{'rs'} = '|';
-    $$p_win{'ts'} = '-';
-    $$p_win{'bs'} = '-';
-    $$p_win{'tl'} = '+';
-    $$p_win{'tr'} = '+';
-    $$p_win{'bl'} = '+';
-    $$p_win{'br'} = '+';
+    $p_win->{'height'} = 3;
+    $p_win->{'width'} = 10;
+    $p_win->{'starty'} = ($LINES - $p_win->{'height'}) / 2;
+    $p_win->{'startx'} = ($COLS - $p_win->{'width'}) / 2;
+    $p_win->{'ls'} = '|';
+    $p_win->{'rs'} = '|';
+    $p_win->{'ts'} = '-';
+    $p_win->{'bs'} = '-';
+    $p_win->{'tl'} = '+';
+    $p_win->{'tr'} = '+';
+    $p_win->{'bl'} = '+';
+    $p_win->{'br'} = '+';
 }
 
 sub print_win_params {
     if ($_DEBUG) {
-	$p_win = shift;
+	my $p_win = shift;
 	addstr(25, 0,
 	    "$$p_win{startx} $$p_win{starty} $$p_win{width} $$p_win{height}");
 	refresh();
@@ -82,13 +85,13 @@ sub print_win_params {
 }
 
 sub create_box {
-    $p_win = shift;
-    $bool = shift;
+    my $p_win = shift;
+    my $bool = shift;
 
-    $x = $$p_win{'startx'};
-    $y = $$p_win{'starty'};
-    $w = $$p_win{'width'};
-    $h = $$p_win{'height'};
+    my $x = $$p_win{'startx'};
+    my $y = $$p_win{'starty'};
+    my $w = $$p_win{'width'};
+    my $h = $$p_win{'height'};
 
     if ($bool) {
 	addch($y, $x, $$p_win{'tl'});
@@ -101,6 +104,7 @@ sub create_box {
 	vline($y + 1, $x + $w, $$p_win{'rs'}, $h - 1);
     }
     else {
+	my ($j, $i);
 	for ($j = $y; $j <= $y + $h; $j++) {
 	    for ($i = $x; $i <= $x + $w; $i++) {
 		addch($j, $i, ' ');
